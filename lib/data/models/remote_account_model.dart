327b2f5eb38d3674cbe2f61331450bd9f772d9a8
Samuel Campos <srctrabalho@gmail.com>
2021-09-09 19:47:58 -0300

    move duplicated code to helper method

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,12 +1,17 @@
-import 'package:fordev/domain/entities/entities.dart';
+import '../../domain/entities/entities.dart';
+
+import '../http/http.dart';
 
 class RemoteAccountModel {
   final String acessToken;
 
   RemoteAccountModel(this.acessToken);
 
-  factory RemoteAccountModel.fromJson(Map json) =>
-      RemoteAccountModel(json['acessToken']);
-
+  factory RemoteAccountModel.fromJson(Map json) {
+    if (!json.containsKey('acessToken')) {
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(json['acessToken']);
+  }
   AccountEntity toEntity() => AccountEntity(acessToken);
 }
