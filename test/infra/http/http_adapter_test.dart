1e6509b83d7ba2cecbc9915a75883b1ab21f4d95
Samuel Campos <srctrabalho@gmail.com>
2021-09-14 15:49:32 -0300

    ensure HttpAdapter calls post without Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,7 +20,8 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(Uri.parse(url), headers: headers, body: jsonBody);
   }
 }
 
@@ -48,6 +49,11 @@ void main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}'));
+    });
+    test('Should call post without body', () async {
+      await sut.request(url: any, method: 'post');
+
+      verify(client.post(Uri.parse(url), headers: anyNamed('headers')));
     });
   });
 }
